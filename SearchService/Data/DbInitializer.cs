99bc183cfd539a4c4f1595ddf74c1c5fbe427ed0
ngwueche <wisdomngwueche@gmail.com>
2024-01-26 15:53:32 +0100

    added search service

---
+++ SearchService/Data/DbInitializer.cs
@@ -0,0 +1,6 @@
+﻿namespace SearchService.Data
+{
+    public class DbInitializer
+    {
+    }
+}
--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 
-await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
 var app = builder.Build();
 
 
@@ -19,5 +19,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
