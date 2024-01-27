3e9c2439d7542b088c8af7cfac99fb9a14245e0b
ngwueche <wisdomngwueche@gmail.com>
2024-01-27 15:09:35 +0100

    implemented pagination

--- SearchService/Controllers/SearchController.cs
+++ SearchService/Controllers/SearchController.cs
@@ -11,15 +11,23 @@ namespace SearchService.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>().Sort(x => x.Ascending(a => a.Make));
+            var query = DB.PagedSearch<Item>().Sort(x => x.Ascending(a => a.Make)); //PageSearch enables Pagination
             //search function
-            if(!string.IsNullOrEmpty(searchTerm))
+            if (!string.IsNullOrEmpty(searchTerm))
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
 
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+
             var result = await query.ExecuteAsync();
-            return result;
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
         }
     }
 }
