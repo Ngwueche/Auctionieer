3f3cf49b1e5e8ee2bb432aa7c850e76a8ebf9d9d
ngwueche <wisdomngwueche@gmail.com>
2024-01-28 07:49:53 +0100

    Connecting Auction and serach services

---
+++ SearchService/Services/AuctionServiceHTTPClient.cs
@@ -0,0 +1,14 @@
+﻿namespace SearchService.Services
+{
+    public class AuctionServiceHTTPClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public AuctionServiceHTTPClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+    }
+}
