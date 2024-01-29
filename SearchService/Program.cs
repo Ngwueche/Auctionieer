6de4bbc942bd42304253baca3505c42de12143c7
ngwueche <wisdomngwueche@gmail.com>
2024-01-29 23:19:14 +0100

    added polloing for making continues request

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,14 +1,17 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHTTPClient>();
+builder.Services.AddHttpClient<AuctionServiceHTTPClient>().AddPolicyHandler(GetPolicy());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
@@ -23,14 +26,26 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
 
-    Console.WriteLine(e); ;
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine(e); ;
+    }
+
+});
 
 app.Run();
+
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
